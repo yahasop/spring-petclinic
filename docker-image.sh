01b89eea620bb4802495c7ebc98339c78cbcc90e
Yamil Solis <yamil@email.com>
2024-12-06 11:42:35 -0600

    changes to sh

--- docker-image.sh
+++ docker-image.sh
@@ -1,7 +1,6 @@
 sudo usermod -aG docker jenkins
-sudo usermod -aG docker $USER
 sudo newgrp docker
-ECR_URL="$(aws ecr describe-repositories | jq -r .repositories[0].repositoryUri | cut -d \'/\' -f 1)"
+ECR_URL="$(aws ecr describe-repositories | jq -r .repositories[0].repositoryUri | cut -d '/' -f 1)"
 ECR_NAME="$(aws ecr describe-repositories | jq -r .repositories[0].repositoryName)"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_URL
 docker build -t $ECR_NAME .
