9c5a4605058b42a61ae240b771bc6f1be16555d9
Yamil Solis <yamil@email.com>
2024-12-14 16:27:27 -0600

    some files added

---
+++ create-inv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+LINES=()
+
+while IFS= read -ra line; do
+        LINES+=("$line")
+done < instances-ip.txt
+
+FIRST=${LINES[0]}
+SECOND=${LINES[1]}
+
+cat <<EOF > ./ansible/hosts
+[appserver]
+appserver1 ansible_host=$FIRST ansible_ssh_user=ubuntu
+
+[nexuserver]
+nexuserver1 ansible_host=$SECOND ansible_ssh_user=ubuntu
+EOF
+
+cat <<EOF > /etc/ansible/ansible.cfg
+[defaults]
+host_key_checking=False
+EOF
