1f12f18eeb6f37d2257b550b6694685a6fdb26fd
Yamil Solis <yamil@email.com>
2024-12-16 14:51:56 -0600

    sh files modified

--- ansible/docker-pulling-image.sh
+++ ansible/docker-pulling-image.sh
@@ -1,5 +1,5 @@
-sudo usermod -aG docker jenkins
-sudo newgrp docker
+#sudo usermod -aG docker jenkins
+#sudo newgrp docker
 ECR_URL="$(aws ecr describe-repositories | jq -r .repositories[0].repositoryUri | cut -d '/' -f 1)"
 ECR_NAME="$(aws ecr describe-repositories | jq -r .repositories[0].repositoryName)"
 aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin $ECR_URL
